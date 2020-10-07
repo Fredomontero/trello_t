3161f28b89519fe33a2991e18d819c67b4d02e47
Fredomontero <christianmonntero08@gmail.com>
2020-10-06 22:28:40 -0700

    Feat: integrate dashboard into App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import './App.css';
+import { Dashboard } from './pages/Dashboard/Dashboard.page';
 
 function App() {
   return (
     <div id="app-container">
       <h1>This is gonna be the App title</h1>
+      <Dashboard/>
     </div>
   );
 }
