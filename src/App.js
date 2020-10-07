744a119d827a1d0f73508026137a37bad528d8ce
Fredomontero <christianmontero08@gmail.com>
2020-10-06 22:31:19 -0700

    Feat: Integrate Login into app

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import './App.css';
 import { Dashboard } from './pages/Dashboard/Dashboard.page';
+import { Login } from './pages/Login/Login.page';
 
 function App() {
   return (
     <div id="app-container">
       <h1>This is gonna be the App title</h1>
-      <Dashboard/>
+      <Login/>
     </div>
   );
 }
